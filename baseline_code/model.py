Fix the issue of numerical stability

baseline_code/model.py
@@ -19,26 +19,26 @@ class SEModel(L.LightningModule):
         self.se_model = BSRNN()
         self.mr_l1_loss = MultiResL1SpecLoss(window_sz=[256, 512, 768, 1024], eps = 1.0e-6,normalize_variance=True, time_domain_weight=0.5)
         self.sisnr_loss = SISNRLoss()
-        self.grad_has_nan = False
 
     def on_before_optimizer_step(self, optimizer: Optimizer) -> None:
-
         return
 
     def on_after_backward(self):
+        return
+    
+    def optimizer_step(self, epoch, batch_idx, optimizer, optimizer_closure=None):
+
         # check if has grad of NaN
-        self.grad_has_nan = any(
+        grad_has_nan = any(
             torch.isnan(p.grad).any() 
             for p in self.parameters() 
             if p.grad is not None
         )
-
-    
-    def optimizer_step(self, epoch, batch_idx, optimizer, optimizer_closure=None):
-        if self.grad_has_nan:
-            print('NaN in grad has been decected, reset grad to zero')
+        if grad_has_nan:
+            rank = torch.distributed.get_rank()
+            print(f'RANK {rank}: NaN in grad has been decected, reset grad to zero')
             optimizer.zero_grad()
-            self.grad_has_nan = False
+            
         super().optimizer_step(epoch, batch_idx, optimizer, optimizer_closure)
     
     def forward_step(self, batch, stage='train'):